cad1449da2cee3595368e555de8f75d08f56fd73
unknown <BayramovSardor@gmail.com>
2020-04-04 04:43:49 +0500

    registration flow improvements

--- bot.php
+++ bot.php
@@ -35,13 +35,39 @@
 	// VARIABLES
 	$name = "";
 	$age = "";
-	$city = "";
+	$region = "";
 	$address = "";
 	$school = "";
 	$level = "";
+	$phone = ""
+
+	// VALIDATION
+	$age_pattern = "";
+	$is_region =
+		  ($message == "Toshkent shahri")
+		||($message == "Toshkent viloyati")
+		||($message == "Andijon")
+		||($message == "Buxoro")
+		||($message == "Jizzax")
+		||($message == "Farg'ona")
+		||($message == "Qashqadaryo")
+		||($message == "Xorazm")
+		||($message == "Namangan")
+		||($message == "Navoiy")
+		||($message == "Samarqand")
+		||($message == "Surxondaryo")
+		||($message == "Sirdaryo")
+		||($message == "Qoraqalpog'iston");
+
+	$is_level =
+		  ($message == "9 - sinf")
+		||($message == "10 - sinf")
+		||($message == "11 - sinf")
+		||($message == "Talaba");
+
+	$phone_pattern;
 
 	// ADDING USER TO USER TABLE
-	add_user();
 
 
 	// TELEGRAM BUTTONS
@@ -65,6 +91,40 @@
 		[
 			[
 				["text" => "Bekor qilish"]
+			]
+		]
+	]);
+
+	$regions = json_encode([
+		"resize_keyboard" => true,
+		"keyboard" =>
+		[
+			[
+				["text" => "Toshkent shahri"],
+				["text" => "Toshkent viloyati"]
+			],
+			[
+				["text" => "Andijon"],
+				["text" => "Buxoro"],
+				["text" => "Jizzax"]
+			],
+			[
+				["text" => "Farg'ona"],
+				["text" => "Qashqadaryo"],
+				["text" => "Xorazm"]
+			],
+			[
+				["text" => "Namangan"],
+				["text" => "Navoiy"],
+				["text" => "Samarqand"]
+			],
+			[
+				["text" => "Surxondaryo"],
+				["text" => "Sirdaryo"],
+				["text" => "Qoraqalpog'iston"]
+			],
+			[
+				["text" => "Bekor qlish"]
 			]
 		]
 	]);
@@ -102,6 +162,7 @@
 	if(isset($message)) {
 		typing();
 
+		// => REGISTER
 		if($message == "👥Ro'yxatdan o'tish") {
 			mkdir("temp/$chat_id");
 			file_put_contents("temp/$chat_id/step.log", "0");
@@ -115,6 +176,8 @@
 
 		else if(is_dir("temp/$chat_id")) {
 			$step = file_get_contents("$chat_id/step.log");
+
+			// => NAME
 			if($step == "0") {
 				if($message == "Bekor qilish") {
 					delete_files();
@@ -122,100 +185,223 @@
 						"chat_id" => $chat_id,
 						"text" => "Ro'yxat bekor qilindi!",
 						"parse_mode" => "markdown",
-						"reply_markup" => $buttons
+						"reply_markup" => $default
 					]);
 				} else {
 					file_put_contents("$chat_id/step.log", "1");
 					file_put_contents("$chat_id/name.log", $message);
 					bot("sendMessage", [
 						"chat_id" => $chat_id,
-						"text" => "Nechinchi sinf o'quvchisisiz?",
-						"parse_mode" => "markdown",
-						"reply_markup" => $level
-					]);
-				}
-			}
-			else if(file_get_contents("$chat_id/step.log") == "1") {
-				if($message == "Bekor qilish") {
-					delete_files($chat_id);
-					bot("sendMessage", [
-						"chat_id" => $chat_id,
-						"text" => "Ro'yxat bekor qilindi!",
-						"parse_mode" => "markdown",
-						"reply_markup" => $buttons
-					]);
-				} else {
-					file_put_contents("$chat_id/step.log", "2");
-					file_put_contents("$chat_id/level.log", $message);
-					bot("sendMessage", [
-						"chat_id" => $chat_id,
-						"text" => "📞 Telefon raqamingizni kiriting: ",
+						"text" => "Yoshingiz nechada?",
 						"parse_mode" => "markdown",
 						"reply_markup" => $cancel
 					]);
 				}
 			}
-			else if(file_get_contents("$chat_id/step.log") == "2") {
+
+			// => AGE
+			else if($step == "1") {
 				if($message == "Bekor qilish") {
-					delete_files($chat_id);
+					delete_files();
 					bot("sendMessage", [
 						"chat_id" => $chat_id,
 						"text" => "Ro'yxat bekor qilindi!",
 						"parse_mode" => "markdown",
-						"reply_markup" => $buttons
+						"reply_markup" => $default
 					]);
-				} else {
-					file_put_contents("$chat_id/step.log", "3");
-					file_put_contents("$chat_id/phone.log", $message);
-					$name = file_get_contents("$chat_id/name.log");
-					$level = file_get_contents("$chat_id/level.log");
-					$phone = file_get_contents("$chat_id/phone.log");
+				} else if(preg_match($age_pattern, $message)) {
+					file_put_contents("temp/$chat_id/step.log", "2");
+					file_put_contents("temp/$chat_id/age.log", $message);
 					bot("sendMessage", [
 						"chat_id" => $chat_id,
-						"text" =>  "Ma'lumotlarni tasdiqlaysizmi?\n\nIsmingiz: $name\nToifangiz: $level\nTelefon Raqamingiz: $phone\n",
+						"text" => "Iltimos Shahringizni tanlang.\n Quyidagi tugmalardan birini bosing.",
 						"parse_mode" => "markdown",
-						"reply_markup" => $yes_no
+						"reply_markup" => $regions
+					]);
+				} else {
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Noto'g'ri format! \n Iltimos faqat raqamlar bilan yozing!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $cancel
 					]);
 				}
 			}
-			else if(file_get_contents("$chat_id/step.log") == "3") {
+
+			// => REGION
+			else if($step == "2") {
 				if($message == "Bekor qilish") {
-					delete_files($chat_id);
+					delete_files();
 					bot("sendMessage", [
 						"chat_id" => $chat_id,
 						"text" => "Ro'yxat bekor qilindi!",
 						"parse_mode" => "markdown",
-						"reply_markup" => $buttons
+						"reply_markup" => $default
 					]);
-				} else if($message == "Yuborish") {
-					$name = file_get_contents("$chat_id/name.log");
-					$level = file_get_contents("$chat_id/level.log");
-					$phone = file_get_contents("$chat_id/phone.log");
-					upload($name, $level, $phone);
-					delete_files($chat_id);
+				} else if($is_region) {
+					file_put_contents("temp/$chat_id/step.log", "3");
+					file_put_contents("temp/$chat_id/region.log", $message);
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" =>  "Toifangizni tanlang. \n Quyidagi tugmalardan birini bosing.",
+						"parse_mode" => "markdown",
+						"reply_markup" => $level
+					]);
+				} else {
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" =>  "Noto'g'ri format. \n Iltimos quyidagi tugmalardan birini bosing!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $regions
+					]);
+				}
+			}
+
+			// => LEVEL
+			else if($step == "3") {
+				if($message == "Bekor qilish") {
+					delete_files();
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Ro'yxat bekor qilindi!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $default
+					]);
+				} else if($is_level) {
+					file_put_contents("temp/$chat_id/step.log", "4");
+					file_put_contents("temp/$chat_id/level.log", $message);
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Iltimos o'quv dargohingizning nomini kiriting.",
+						"parse_mode" => "markdown",
+						"reply_markup" => $cancel
+					]);
+				} else {
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" =>  "Noto'g'ri format. \n Iltimos quyidagi tugmalardan birini bosing!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $regions
+					]);
+				}
+			}
+
+			// => SCHOOL
+			else if($step == "4") {
+				if($message == "Bekor qilish") {
+					delete_files();
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Ro'yxat bekor qilindi!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $default
+					]);
+				} else {
+					file_put_contents("temp/$chat_id/step.log", "5");
+					file_put_contents("temp/$chat_id/school.log", $message);
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Telefon raqamingizni kiriting",
+						"parse_mode" => "markdown",
+						"reply_markup" => $calcel
+					]);
+				}
+			}
+
+			// => PHONE
+			else if($step == "5") {
+				if($message == "Bekor qilish") {
+					delete_files();
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Ro'yxat bekor qilindi!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $default
+					]);
+				} else if(preg_match($phone_pattern, $message)) {
+					file_put_contents("temp/$chat_id/step.log", "6");
+					file_put_contents("temp/$chat_id/phone.log", $message);
+
+					$name = file_get_contents("temp/chat_id/name.log");
+					$age = file_get_contents("temp/chat_id/age.log");
+					$region = file_get_contents("temp/chat_id/region.log");
+					$level = file_get_contents("temp/chat_id/level.log");
+					$school = file_get_contents("temp/chat_id/school.log");
+					$phone = file_get_contents("temp/chat_id/phone.log")
+
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "
+							**Malumotlarni Tasdiqlaysizmi?**\n
+							1. F.I.Sh:				*$name*\n
+							2. Yoshingiz:			*$age*\n
+							3. Shahringiz:			*$region*\n
+							4. Toifangiz:			*$level*\n
+							5. O'quv muassassasi:	*$school*\n
+							6. Telefon raqamingiz:	*$phone*\n",
+						"parse_mode" => "markdown",
+						"reply_markup" => $submit
+					]);
+				} else {
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Noto'g'ri format. \n Faqat sonlardan foydalanish mumkin!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $submit
+					]);
+				}
+			}
+			// => SUBMIT
+			else if($step == "6") {
+				if($message == "Bekor qilish") {
+					delete_files();
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Ro'yxat bekor qilindi!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $default
+					]);
+				} else if($message == "Tasdiqlash") {
+					$name = file_get_contents("temp/chat_id/name.log");
+					$age = file_get_contents("temp/chat_id/age.log");
+					$region = file_get_contents("temp/chat_id/region.log");
+					$level = file_get_contents("temp/chat_id/level.log");
+					$school = file_get_contents("temp/chat_id/school.log");
+					$phone = file_get_contents("temp/chat_id/phone.log")
+
+
+
 					bot("sendMessage", [
 						"chat_id" => $chat_id,
 						"text" => "✅ Ro'yxatdan o'tish muvaffaqiyatli yakunlandi!",
 						"parse_mode" => "markdown",
-						"reply_markup" => $buttons
+						"reply_markup" => $submit
+					]);
+				} else {
+					bot("sendMessage", [
+						"chat_id" => $chat_id,
+						"text" => "Iltimos quyidagi tugmalardan birini bosing!",
+						"parse_mode" => "markdown",
+						"reply_markup" => $submit
 					]);
 				}
 			}
 		}
 		//	********************************************************************
 		else if($message == "/start") {
-			seen();
+			add_user();
+
 			bot("sendMessage", [
 				"chat_id" => $chat_id,
 				"text" => "Assalomu Alaykum!",
 				"parse_mode" => "markdown",
-				"reply_markup" => $buttons
+				"reply_markup" => $default
 			]);
 			bot("sendMessage", [
 				"chat_id" => $chat_id,
 				"text" => "<b>\"Muhammad al-Xorazmiy\"</b> nomidagi axborot texnologiyalari va <b>\"O'zbekiston Matematiklar va Informatika Assotsiatsiyasi\"</b> tashabbusi bilan birga tashkil etilgan matematika olimpiadasi qabuliga xush kelibsiz!",
 				"parse_mode" => "html",
-				"reply_markup" => $buttons
+				"reply_markup" => $default
 			]);
 		}
 
@@ -224,13 +410,13 @@
 				"chat_id" => $chat_id,
 				"text" => "📌Musobaqa manzili:",
 				"parse_mode" => "markdown",
-				"reply_markup" => $buttons
+				"reply_markup" => $default
 			]);
 			bot("sendLocation", [
 				"chat_id" => $chat_id,
 				"latitude" => "41.302632",
 				"longitude" => "69.315566",
-				"reply_markup" => $buttons
+				"reply_markup" => $default
 			]);
 		}
 
@@ -239,16 +425,7 @@
 				"chat_id" => $chat_id,
 				"text" => "📥 <b>Biz bilan bog'lanish:</b> \n\n📞 Tel.: <a href=\"tel:998712670027\">+998 71-267-00-27</a>\n🌐 Telegram: <a href=\"https://t.me/uzmia31\">UZMIA</a>",
 				"parse_mode" => "html",
-				"reply_markup" => $buttons
-			]);
-		}
-
-		else if($message == "Bekor qilish") {
-			bot("sendMessage", [
-				"chat_id" => $chat_id,
-				"text" => "Ro'yxatdan o'tish bekor qilindi!",
-				"parse_mode" => "markdown",
-				"reply_markup" => $buttons
+				"reply_markup" => $default
 			]);
 		}
 
@@ -257,7 +434,7 @@
 				"chat_id" => $chat_id,
 				"text" => "Iltimos, quyidagi tugmalardan birini bosing!",
 				"parse_mode" => "markdown",
-				"reply_markup" => $buttons
+				"reply_markup" => $default
 			]);
 		}
 	}
--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <html>
 <head>
 	<title>UZMIA - Registration</title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css"/>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css"/>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
 	<div class="container">
