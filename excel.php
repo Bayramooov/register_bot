7822825eb60eb5805b532ed98c464414e1f8e231
unknown <BayramovSardor@gmail.com>
2020-04-08 01:36:05 +0500

    Export to excel feature rewritten

--- excel.php
+++ excel.php
@@ -1,10 +1,23 @@
 <?php
-	$output = file_get_contents("table.html");
+	require_once("private.php");
+	require_once("funcs.php");
+	$con = mysqli_connect(
+		DB_HOST,
+		DB_USERNAME,
+		DB_PASSWORD,
+		DB_NAME
+	);
+	if(!$con)
+		die("Kechirasiz texnik nosozlik sodir bo'ldi. Bu haqda texnik hodimlarga habar jo'natildi. Iltimos birozdan keyin habar oling.");
+
+	$table = "<table>";
+	$table .= candidates($con);
+
 	if(isset($_POST["export"])) {
 		header("Content-Type:application/xls");
 		header("Content-Disposition:attachment;filename=\"candidates.xls\"");
-		$output .= "</table>";
-		echo $output;
+		$table .= "</table>";
+		echo $table;
 		die();
 	} echo "error accured!";
 ?>
