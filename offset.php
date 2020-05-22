27a6763106ec37f7677b7b02a664e73e67bada3c
unknown <BayramovSardor@gmail.com>
2020-05-22 17:08:37 +0500

    clening queue when overflowed

---
+++ offset.php
@@ -0,0 +1,22 @@
+<?php
+	require_once("settings.php");
+	require_once("funcs.php");
+
+	file_get_contents(DELETE_WEBHOOK);
+
+	$update = json_decode(file_get_contents(GET_UPDATES), true);
+	
+	while (true) {
+		$update_id = end($update['result']);
+		if (!isset($update_id['update_id']))
+			break;
+		$update_id = $update_id['update_id'];
+		$update_id++;
+		$update = offset($update_id);
+		sleep(0.1);
+	}
+
+	$update = json_decode(file_get_contents(SET_WEBHOOK), true);
+	print_r($update);
+	echo "<br><br>Success";
+?>
